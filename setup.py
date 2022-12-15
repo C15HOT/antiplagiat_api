d92eef3b5ad49e4033c8edaba1a3b6229f00a7b3
C15HOT <wvxp@mail.ru>
2022-12-15 16:13:12 +0300

    setup

---
+++ setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+with open('requirements_dev.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name="notebookc",
+    version="0.0.1",
+    author="C15HOT",
+    author_email="wvxp@mail.ru",
+    description="Antiplagiat API",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/C15HOT/antiplagiat_api",
+    packages=find_packages(),
+    install_requires=required,
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
